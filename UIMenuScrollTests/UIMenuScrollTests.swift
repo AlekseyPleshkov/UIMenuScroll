c04bcb107a6eb9fe2bcde87c029937efe7d270b3
Aleksey Pleshkov <im@alekseypleshkov.ru>
2018-10-13 12:56:38 +0300

    Release 0.0.2

--- UIMenuScrollTests/UIMenuScrollTests.swift
+++ UIMenuScrollTests/UIMenuScrollTests.swift
@@ -12,6 +12,7 @@ import XCTest
 class UIMenuScrollTests: XCTestCase {
 
     override func setUp() {
+        UIMenuScrollView()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
